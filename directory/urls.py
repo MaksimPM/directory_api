2bf8eade3d4620ef616ec9d78ce61fd7307e77a7
maksim <mavel.production23@gmail.com>
2024-11-14 23:41:25 +0300

    update urls.py

--- directory/urls.py
+++ directory/urls.py
@@ -2,15 +2,17 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
 from directory.apps import DirectoryConfig
-from directory.views import MaterialViewSet, CategoryViewSet
+from .views import MaterialViewSet, CategoryViewSet, UploadMaterialsView, CategoryWithCostViewSet
 
 
 app_name = DirectoryConfig.name
 
 router = DefaultRouter()
-router.register(r'materials', MaterialViewSet)
-router.register(r'categories', CategoryViewSet)
+router.register(r'materials', MaterialViewSet, basename='material')
+router.register(r'categories', CategoryViewSet, basename='category')
+router.register(r'categories_with_cost', CategoryWithCostViewSet, basename='category_with_cost')
 
 urlpatterns = [
+    path('upload_materials/', UploadMaterialsView.as_view(), name='upload_materials'),
     path('', include(router.urls)),
 ]
