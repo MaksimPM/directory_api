6ba98844236d881be61e5e242eeaa8550324dc2d
maksim <mavel.production23@gmail.com>
2024-11-15 22:07:39 +0300

    fix views.py

--- directory/views.py
+++ directory/views.py
@@ -27,25 +27,21 @@ class UploadMaterialsView(APIView):
         if not file:
             return Response({"error": "No file provided"}, status=status.HTTP_400_BAD_REQUEST)
 
-        # Загружаем Excel файл
         workbook = load_workbook(file)
         sheet = workbook.active
 
-        # Проверяем, что файл содержит нужные заголовки
-        headers = [cell.value for cell in sheet[1]]  # Чтение первой строки как заголовков
+        headers = [cell.value for cell in sheet[1]]
         required_headers = ['Материал', 'Категория', 'Код материала', 'Стоимость материала']
 
         if not all(header in headers for header in required_headers):
             return Response({"error": "Invalid Excel file format, missing required headers."},
                             status=status.HTTP_400_BAD_REQUEST)
 
-        # Индексы столбцов
         material_index = headers.index('Материал')
         category_index = headers.index('Категория')
         code_index = headers.index('Код материала')
         cost_index = headers.index('Стоимость материала')
 
-        # Чтение данных из Excel файла, начиная со второй строки (минус первая строка с заголовками)
         for row in sheet.iter_rows(min_row=2, values_only=True):
             name = row[material_index]
             category_name = row[category_index]
@@ -53,9 +49,8 @@ class UploadMaterialsView(APIView):
             cost = row[cost_index]
 
             if not name or not category_name or not code or cost is None:
-                continue  # Пропускаем строки с отсутствующими обязательными данными
+                continue
 
-            # Создание категории и материала
             try:
                 category, created = Category.objects.get_or_create(name=category_name)
             except IntegrityError:
@@ -69,5 +64,5 @@ class UploadMaterialsView(APIView):
 
 
 class CategoryWithCostViewSet(viewsets.ModelViewSet):
-    queryset = Category.objects.filter(parent=None)  # Начинаем с корневых категорий
+    queryset = Category.objects.filter(parent=None)
     serializer_class = CategoryWithTotalCostSerializer
